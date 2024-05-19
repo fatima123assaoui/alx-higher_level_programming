7b70cdb3d3962b220ba133a76ff51911a3b4fcd1
fatima123assaoui <fatima.assaoui.19@gmail.com>
2024-05-19 10:34:46 -0700

    K

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define a class named Square to represent a square."""
+
+
+class Square:
+    """A basic class representing a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class named Square to represent a square."""
+
+
+class Square:
+    """Constructor method to initialize a Square object."""
+
+    def __init__(self, size):
+        """Initialize a new Square with the given size."""
+        self.__size = size
